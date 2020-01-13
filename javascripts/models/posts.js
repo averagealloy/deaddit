17e46570ac28d7e6d24203dd7ae80d2a65fd419f
averagealloy <m.mctighe10@gmail.com>
2020-01-13 18:42:32 -0500

    added scripts to html and fixed my posts.js file

--- javascripts/models/posts.js
+++ javascripts/models/posts.js
@@ -1,12 +1,12 @@
-class Posts(){
-    constructor(){
-    this.posts = []
-    this.adapter = new PostsAdapter()
-    this.bindEventListeners()
-    this.fetchAndLoadPosts()
+class Posts {
+    constructor() {
+        this.posts = []
+        this.adapter = new PostsAdapter()
+        // this.bindEventListeners()
+        this.fetchAndLoadPosts()
     }
 
-    fetchAndLoadPosts(){
+    fetchAndLoadPosts() {
         this.adapter.getPosts().then(posts => {
             console.log(posts)
         })
