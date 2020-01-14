3b0660acd6e0fe263f42a0c6f95e81af118c7294
averagealloy <m.mctighe10@gmail.com>
2020-01-14 13:04:43 -0500

    had a typo in my postsadapter

--- javascripts/adapters/PostsAdapter.js
+++ javascripts/adapters/PostsAdapter.js
@@ -1,7 +1,7 @@
 class PostAdapter {
     constructor(){
         this.baseUrl = 
-        'http://localhost:3000/api/num1/notes'
+        'http://localhost:3000/api/num1/posts'
     }
     getPosts(){
         return fetch(this.baseUrl).then(res => res.json()
