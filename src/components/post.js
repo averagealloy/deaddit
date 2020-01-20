24cb86fc3c4616b6e40681bada29523411a45945
averagealloy <m.mctighe10@gmail.com>
2020-01-20 15:12:59 -0500

    took out previous code

--- src/components/post.js
+++ src/components/post.js
@@ -1,20 +1,3 @@
-// class Post {
-//     constructor(postJSON){
-//         this.id = postJSON.id
-//         this.title = postJSON.title
-//         this.body = postJSON.body
-//     }
-
-//     renderLi(){
-//         return` <h2 class="header-title" data-id=${this.id}>${this.title}</h2> 
-//         </br>
-//         <p class="post-body" data-id=${this.id}> ${this.body}</p>`
-//     }
-// }
-
-
-// // contenteditable="true" this is pretty cool 
-
  class Post {
         constructor(postJSON){
             this.id = postJSON.id
--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
 const app = new App()
-// everyhtig needs to be loaded before the instance of a new app is created 
+ 
